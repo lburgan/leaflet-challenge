06b7fc2bfc7d2977a3421917ce4ebce4c88f98db
Layla <layla.burgan@gmail.com>
2023-07-10 11:34:37 -0700

    I have a map...that map has no earthquakes on it

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,6 @@
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-d3.json(url).then(data => console.log(data));
+d3.json(url).then(data => createFeatures(data));
 
 function createMap(earthquakes){
     let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -15,7 +15,7 @@ function createMap(earthquakes){
         "Earthquakes": earthquakes
     };
 
-    let map = L.map("map-id", {
+    let map = L.map("map", {
         center: [37.09, -95.71],
         zoom: 5,
         layers: [streetmap, earthquakes]
@@ -45,14 +45,22 @@ function colorCircle(depth){
 
 };
 
-function createFeatures(earthquakeData){
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-      };
-    function createMarker()
-    let earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature,
+function sizeCircle(mag){
+    return mag*2000;
+};
+function createFeatures(response){
+    let jsonData = response.features;
+    let earthquakes = [];
+    for(let index = 0; index<jsonData.length; index ++){
+        let quake = jsonData[index];
 
-      });
-
-}
+        let quakeMarker = L.marker([quake.geometry.coordinates[0],quake.geometry.coordinates[1]],{
+            radius: sizeCircle(quake.properties.mag),
+            fillColor:  colorCircle(quake.geometry.coordinates[2]),
+            fillOpacity: 0.7,
+            color: "black"
+        }).bindPopup(`<h3>Location: ${quake.properties.place}</h3><hr><p>Date: ${new Date(quake.properties.time)}</p><p>Magnitude: ${quake.properties.mag}</p><p>Depth: ${quake.geometry.coordinates[2]}</p>`);
+        earthquakes.push(quakeMarker);
+    };
+    createMap(L.layerGroup(earthquakes))
+};
